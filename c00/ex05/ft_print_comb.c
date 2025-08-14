6095ff9e46c1c315173b49bda90349c34c15bdea
Abdelouahed Erraji <aerraji@e1r3p4.1337.ma>
2025-08-14 20:56:32 +0100

    fix

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: aerraji <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 22:05:54 by aerraji           #+#    #+#             */
-/*   Updated: 2025/08/14 10:39:10 by aerraji          ###   ########.fr       */
+/*   Updated: 2025/08/14 20:56:10 by aerraji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,14 @@ void	ft_put_three_chars(char a, char b, char c)
 {
 	while (c <= '9')
 	{
-		if (a != b && a != c && b != c)
+		ft_putchar(a);
+		ft_putchar(b);
+		ft_putchar(c);
+		if (a == '7' && b == '8' && c == '9')
 		{
-			ft_putchar(a);
-			ft_putchar(b);
-			ft_putchar(c);
-			if (a == '7' && b == '8' && c == '9')
-			{
-				break ;
-			}
-			write (1, ", ", 2);
+			break ;
 		}
+		write (1, ", ", 2);
 		c++;
 	}
 }
@@ -45,10 +42,10 @@ void	ft_print_comb(void)
 	digit1 = 0 + '0';
 	while (digit1 <= '7')
 	{
-		digit2 = 1 + '0';
+		digit2 = digit1 + 1;
 		while (digit2 <= '8')
 		{
-			digit3 = 2 + '0';
+			digit3 = digit2 + 1;
 			ft_put_three_chars(digit1, digit2, digit3);
 			digit2++;
 		}
