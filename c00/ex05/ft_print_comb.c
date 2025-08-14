8594399ff2fd7529fa24e4b7c49785f4def86e20
Abdelouahed Erraji <aerraji@e1r3p4.1337.ma>
2025-08-14 10:48:27 +0100

    fix ex05

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -6,58 +6,52 @@
 /*   By: aerraji <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 22:05:54 by aerraji           #+#    #+#             */
-/*   Updated: 2025/08/13 23:04:59 by aerraji          ###   ########.fr       */
+/*   Updated: 2025/08/14 10:39:10 by aerraji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void ft_print_comb(void)
+void	ft_put_three_chars(char a, char b, char c)
 {
-	char ci;
-	char ce;
-	char cd;
-	int i;
-	int e;
-	int d;
-
-	i = 0;
-	while (i <= 7)
+	while (c <= '9')
 	{
-		e = 1;
-		ci = i + '0';
-		while (e <= 8)
+		if (a != b && a != c && b != c)
 		{
-			ce = e + '0';
-			d = 2;
-			while (d <= 9)
+			ft_putchar(a);
+			ft_putchar(b);
+			ft_putchar(c);
+			if (a == '7' && b == '8' && c == '9')
 			{
-				if (i != e && i != d && e != d)
-				{
-					cd = d + '0';
-					ft_putchar(ci);
-					ft_putchar(ce);
-					ft_putchar(cd);
-					if (i == 7 && e == 8 && d == 9) {
-						break;
-					}
-					write (1, ", ", 2);
-				}
-				d++;
+				break ;
 			}
-			e++;
+			write (1, ", ", 2);
 		}
-		i++;
+		c++;
 	}
 }
 
-void main()
+void	ft_print_comb(void)
 {
-	ft_print_comb();
-	return;
+	int	digit1;
+	int	digit2;
+	int	digit3;
+
+	digit1 = 0 + '0';
+	while (digit1 <= '7')
+	{
+		digit2 = 1 + '0';
+		while (digit2 <= '8')
+		{
+			digit3 = 2 + '0';
+			ft_put_three_chars(digit1, digit2, digit3);
+			digit2++;
+		}
+		digit1++;
+	}
 }
