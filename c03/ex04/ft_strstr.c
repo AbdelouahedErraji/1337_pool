c56af1b4b409968b2176da2c59443b7c750f20bb
Abdelouahed Erraji <aerraji@e1r5p10.1337.ma>
2025-08-24 19:57:49 +0100

    ff

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,44 +6,49 @@
 /*   By: aerraji <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 15:51:40 by aerraji           #+#    #+#             */
-/*   Updated: 2025/08/23 16:11:53 by aerraji          ###   ########.fr       */
+/*   Updated: 2025/08/24 19:57:28 by aerraji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	length(char *str)
+{
+	int	l;
+
+	l = 0;
+	while (str[l])
+		l++;
+	return (l);
+}
+
+int	found_chars(char *to_find, char *str)
+{
+	int	d;
+
+	d = 0;
+	while (to_find[d] && to_find[d] == str[d])
+		d++;
+	return (d);
+}
 
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	d;
-	int	e;
 	int	l;
 
 	i = 0;
-	l = 0;
-	while (*(to_find + l))
-	{
-		l++;
-	}
+	l = length(to_find);
 	if (l == 0)
-	{
 		return (str);
-	}
 	else
 	{
-		while (*(str + i))
+		while (str[i])
 		{
-			if (*(str + i) == *to_find)
+			if (str[i] == *to_find)
 			{
-				e  = i;
-				d = 0;
-				while (*(to_find + d) && *(to_find + d) == *(str + e))
-				{
-					d++;
-					e++;
-				}
+				d = found_chars(to_find, &str[i]);
 				if (l == d)
-				{
-					return  (str + i);
-				} 
+					return (str[i]);
 			}
 			i++;
 		}
