a3db5cc3d6191e22ae2ef895c207d4df73e8394c
Abdelouahed Erraji <aerraji@e1r5p10.1337.ma>
2025-08-24 20:06:41 +0100

    ex01 c03

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aerraji <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 10:54:37 by aerraji           #+#    #+#             */
-/*   Updated: 2025/08/23 21:03:54 by aerraji          ###   ########.fr       */
+/*   Updated: 2025/08/24 20:06:10 by aerraji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,9 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	if (n == 0)
-		return 0;
-	while (i < n - 1 && s1[i] && s2[i] && s1[i] == s2[i])
+	while (i < n && (s1[i] || s2[i]) && s1[i] == s2[i])
 	{
 		i++;
 	}
-	return (s1[i] - (s2[i]);
+	return (s1[i] - (s2[i]));
 }
