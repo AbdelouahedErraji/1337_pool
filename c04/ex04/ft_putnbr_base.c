12761e5966ad350c15c0c62674040c0fef6f974b
Abdelouahed Erraji <aerraji@e1r5p10.1337.ma>
2025-08-24 16:56:23 +0100

    c04 ex04

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -6,21 +6,57 @@
 /*   By: aerraji <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 20:54:30 by aerraji           #+#    #+#             */
-/*   Updated: 2025/08/23 21:08:31 by aerraji          ###   ########.fr       */
+/*   Updated: 2025/08/24 16:54:56 by aerraji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+int	check_base(char *base)
+{
+	int	i;
+	int	t;
+
+	i = 0;
+	t = 0;
+	while (base[i])
+	{
+		if (base[i] == 43 || base[i] == 45)
+			return (0);
+		while (t < i)
+		{
+			if (base[i] == base[t])
+				return (0);
+			t++;
+		}
+		t = 0;
+		i++;
+	}
+	if (i < 2)
+		return (0);
+	return (i);
+}
+
+void	putnbr_base(int nbr, char *base, int base_size)
+{
+	if (nbr >= base_size)
+		putnbr_base(nbr / base_size, base, base_size);
+	write(1, &base[nbr % base_size], 1);
+}
 
 void	ft_putnbr_base(int nbr, char *base)
 {
 	int	base_size;
 	int	i;
 
-	base_size = 0;
 	i = 0;
-	while (*(base + i))
+	base_size = check_base(base);
+	if (nbr < 0)
 	{
-		base_size++;
+		write(1, "-", 1);
+		nbr *= -1;
 	}
-
-
+	if (base_size == 0)
+		return ;
+	putnbr_base(nbr, base, base_size);
 }
