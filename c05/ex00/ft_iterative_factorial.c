08a71c05962b43eba848195ba0a3710d438fe37b
Abdelouahed Erraji <aerraji@e1r10p11.1337.ma>
2025-08-26 08:15:44 +0100

    fibonacci

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -1,48 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aerraji <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/23 16:12:36 by aerraji           #+#    #+#             */
-/*   Updated: 2025/08/25 15:09:49 by aerraji          ###   ########.fr       */
+/*   Created: 2025/08/25 17:19:24 by aerraji           #+#    #+#             */
+/*   Updated: 2025/08/25 18:33:27 by aerraji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	length(char *c)
+int	ft_iterative_factorial(int nb)
 {
 	int	i;
 
-	i = 0;
-	while (c[i])
+	if (nb < 0)
+		return (0);
+	if (nb == 1)
+		return (1);
+	i = nb;
+	while (i > 1)
 	{
-		i++;
+		i--;
+		nb *= i;
 	}
-	return (i);
-}
-
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
-{
-	unsigned int	sl;
-	unsigned int	dl;
-	unsigned int	i;
-	unsigned int	d;
-
-	sl = length(src);
-	dl = length(dest);
-	i = 0;
-	if (size <= dl)
-	{
-		return (size + sl);
-	}
-	d = dl;
-	while (src[i] && d + 1 < size)
-	{
-		dest[d] = src[i];
-		i++;
-		d++;
-	}
-	dest[d] = '\0';
-	return (sl + dl);
+	return (nb);
 }
