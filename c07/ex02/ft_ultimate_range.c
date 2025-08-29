a597de31ee1d52a505e1df1869103a72686f640c
Abdelouahed Erraji <aerraji@e1r2p10.1337.ma>
2025-08-29 09:36:32 +0100

    ex02

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -1,37 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aerraji <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/29 09:23:09 by aerraji           #+#    #+#             */
-/*   Updated: 2025/08/29 09:23:11 by aerraji          ###   ########.fr       */
+/*   Created: 2025/08/29 09:24:05 by aerraji           #+#    #+#             */
+/*   Updated: 2025/08/29 09:35:26 by aerraji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	*ft_range(int min, int max)
+int	ft_ultimate_range(int **range, int min, int max)
 {
 	int	i;
-	int	*ptr;
 
 	if (min >= max)
 	{
-		return (NULL);
+		*range = NULL;
+		return (0);
 	}
-	ptr = malloc((max - min) * 4);
-	if (!ptr)
+	*range = malloc((max - min) * sizeof(int));
+	if (!*range)
 	{
-		return (NULL);
+		return (-1);
 	}
 	i = 0;
 	while (min < max)
 	{
-		ptr[i] = min;
+		(*range)[i] = min;
 		i++;
 		min++;
 	}
-	return (ptr);
+	return (i);
 }
